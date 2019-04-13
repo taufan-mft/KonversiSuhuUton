5b620ab479d98b2f0cc06bc347fcc5408ae944ef
Taufan <taufanderazor@gmail.com>
2019-04-13 16:57:58 +0700

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,10 +1,11 @@
 Public Class Form1
     'Konversi Rupiah ke Mata Uang lain
+    'yang aku bingung aku tulis di komen ya ton
  
     Private Sub rumus()
         Dim hasil As Integer
-        If txtIDR.Text = "IDR" And txtHasil.Text = "USD" Then
-            hasil = Val(txtIDR.Text) / 14115
+        If txtIDR.Text = "IDR" And txtHasil.Text = "USD" Then  'ini berarti isi txtIDR = "IDR" kan ton
+            hasil = Val(txtIDR.Text) / 14115 'terus ini kenapa nilai txtIDR, yaitu "IDR", dimasukkin rumus? harusnya kan angka. jadi aku menyarankan kode kaya di halaman paling bawah
         ElseIf txtIDR.Text = "IDR" And txtHasil.Text = "Poundsterling" Then
             hasil = Val(txtIDR.Text) / 18373
         ElseIf txtIDR.Text = "IDR" And txtHasil.Text = "Euro" Then
@@ -37,3 +38,22 @@ Public Class Form1
         Call rumus()
     End Sub
 End Class
+
+-----------------------------------------------
+Kode yang saya propose kepada uton:
+
+'Kita punya textbox buat masukin nilai kita kasih nama txtNilai
+
+ Private Sub rumus()
+        Dim hasil As Integer
+    nilai = txtNilai.Text
+        If txtIDR.Text = "IDR" And txtHasil.Text = "USD" Then  
+        hasil = txtNilai.Text / 14115 
+        ElseIf txtIDR.Text = "IDR" And txtHasil.Text = "Poundsterling" Then
+        hasil = txtNilai.Text / 18373
+        ElseIf txtIDR.Text = "IDR" And txtHasil.Text = "Euro" Then
+        hasil = txtNilai.Text / 15977
+        End If
+        txtHasil.Text = hasil
+    End Sub
+
